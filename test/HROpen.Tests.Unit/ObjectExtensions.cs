f55266abfb0b7976df6dfdc3d2e64c95481f979b
Thomas Adams <tadams1138@hotmail.com>
2021-09-28 15:23:14 -0400

    refactor

---
+++ test/HROpen.Tests.Unit/ObjectExtensions.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+
+namespace HROpen
+{
+    public static class ObjectExtensions
+    {
+        private static readonly JsonSerializer JsonSerializer = new()
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver()
+        };
+
+        public static void ShouldMatchJson(this object o, string json)
+        {
+            var t1 = JObject.FromObject(o!, JsonSerializer);
+            var t2 = JObject.Parse(json);
+            JToken.DeepEquals(t1, t2).Should().BeTrue("{0} didn't match {1}", t1, json);
+        }
+    }
+}
---
+++ test/HROpen.Tests.Unit/Screening/SerializationTests.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace HROpen.Screening
+{
+    public class SerializationTests
+    {
+        [Theory]
+        [InlineData("Screening\\json\\samples\\Position.json", typeof(PositionDto))]
+        public async Task SerializesToAndFromJson(string sampleFilePath, Type type)
+        {
+            // Arrange
+            var sample = await Samples.ReadAsync(sampleFilePath);
+
+            // Act
+            var dto = JsonConvert.DeserializeObject(sample, type);
+
+            // Assert
+            dto.ShouldMatchJson(sample);
+        }
+    }
+}
